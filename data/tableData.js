f66d28e32379d1f5ad8a5a9c5a8a8ef5891ca8ff
Eva Hernandez <33330852+EvaH98@users.noreply.github.com>
2018-04-24 20:06:43 -0700

    Update tableData.js

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+var tableArray = [
+	{
+	customerName: "name",
+    customerEmail: "name@example.com",
+    customerID: "nameuser4",
+    phoneNumber: "000-000-0000"
+	}
+];
+
+module.exports = tableArray;
