2f0f486e578b3e055f5d7f9dc7e10990d40f6c64
nvidya.karthik <nvidya.karthik@gmail.com>
2018-04-24 20:41:30 -0700

    working code with all links

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path")
+var path = require("path");
+var tableArray=require("./data/tableData.js");
+var waitingArray=require("./data/waitinglistData.js");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -10,9 +12,27 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./routes/apiRoutes.js");
-require("./routes/htmlRoutes.js");
+//require("/routes/apiRoutes.js");
+
+//require("./routes/htmlRoutes.js");
+ app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/home.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/tables.html"));
+});
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/reserve.html"));
+});
+app.get("/api/tables", function(req, res) {
+
+    return res.json(tableArray);
+});
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitingArray);
+});  
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
-});
+}); 
