17ac9739e8e214d1f2dbb87ceeaa9c9a3061e100
Eva Hernandez <33330852+EvaH98@users.noreply.github.com>
2018-04-24 20:07:59 -0700

    Update waitinglistData.js

---
+++ data/waitinglistData.js
@@ -0,0 +1,11 @@
+var waitingArray = [
+	{
+	customerName: "name2",
+    customerEmail: "name2@example.com",
+    customerID: "nameuser8",
+    phoneNumber: "000-000-0000"
+	}
+];
+
+
+module.exports = waitingArray;
